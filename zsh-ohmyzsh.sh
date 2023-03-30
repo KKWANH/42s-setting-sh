591baf578b267ffe967c4238e74657cbc8800cee
Kwanho Kim <kkim@cx1r3s3.42seoul.kr>
2023-03-30 19:57:05 +0900

    :sparkles: [feature] zsh-ohmyzsh: plugins

--- zsh-ohmyzsh.sh
+++ zsh-ohmyzsh.sh
@@ -1,2 +1,8 @@
 #/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+echo "source ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+echo "source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+
+# cat ~/.zshrc | grep -n "plugins=(git)" | awk -F ':' '{ print $1 }'
+echo "plugins=(git zsh-syntax-highlighting zsh-autosuggestions)"
